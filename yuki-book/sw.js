25e51c14a12ebfcb7cb077c262929a81980e6bcb
eiko07131-max <eiko07131@gmail.com>
2026-09-21 10:07:57 +0900

    Fix snow book reader cache

--- yuki-book/sw.js
+++ yuki-book/sw.js
@@ -1,1 +1,1 @@
-const C='yuki-book-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./reader.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='yuki-book-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./reader.js?v=2','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
